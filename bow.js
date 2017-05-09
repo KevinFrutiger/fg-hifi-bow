e260066988e472f97508b10f9b79b1ec5a553b64
Kevin Frutiger <github@frutigergroup.com>
2017-05-09 05:59:56 -0700

    Remove JSON formatting

--- bow.js
+++ bow.js
@@ -56,12 +56,12 @@
       parentID: this.entityID,
       collisionless: true,
       ignoreForCollisions: 1,
-      dimensions: { "x": 5, "y": 5, "z": 5 }, // Has to be here for string to show. Not sure why it's 5 x 5 x 5. 1 x 1 x 1 doesn't work.
-      linePoints: [ { "x": 0, "y": 0, "z": 0 }, { "x": 0, "y": -1.2, "z": 0 } ],
+      dimensions: { x: 5, y: 5, z: 5 }, // Has to be here for string to show. Not sure why it's 5 x 5 x 5. 1 x 1 x 1 doesn't work.
+      linePoints: [ { x: 0, y: 0, z: 0 }, { x: 0, y: -1.2, z: 0 } ],
       lineWidth: 10,
       color: { red: 153, green: 102, blue: 51 },
-      localPosition: { "x": 0, "y": 0.6, "z": 0.1 }, // Not documented, but needed to put in correct position.
-      localRotation: { "w": 1, "x": 0, "y": 0, "z": 0 }, // Not documented. Doesn't seem to have any affect.
+      localPosition: { x: 0, y: 0.6, z: 0.1 }, // Not documented, but needed to put in correct position.
+      localRotation: { w: 1, x: 0, y: 0, z: 0 }, // Not documented. Doesn't seem to have any affect.
       userData: JSON.stringify({
         grabbableKey: {
           grabbable: false
--- spawnBow.js
+++ spawnBow.js
@@ -1,15 +1,15 @@
 var userData = { // These keys are features in the API
-  "grabbableKey": {
-    "grabbable": true
+  grabbableKey: {
+    grabbable: true
   },
-  "wearable": { // keep on joint when trigger released
-    "joints": {
-      "LeftHand": [
-        {"x": -0.04, "y": 0.02, "z": 0.03}, // position
+  wearable: { // keep on joint when trigger released
+    joints: {
+      LeftHand: [
+        {x: -0.04, y: 0.02, z: 0.03}, // position
         Quat.fromPitchYawRollDegrees(90, -90, 0) // rotation
       ],
-      "RightHand": [
-        {"x": 0.04, "y": 0.02, "z": 0.03},
+      RightHand: [
+        {x: 0.04, y: 0.02, z: 0.03},
         Quat.fromPitchYawRollDegrees(90, 90, 0)
       ]
     }
@@ -20,37 +20,37 @@ const CREATION_DATE = new Date();
 
 // Create the bow
 var id = Entities.addEntity({
-  "name": "Hifi-Bow",
-  "created": CREATION_DATE.toISOString(),
-  "type": "Model",
-  "modelURL": Script.resolvePath("bow-deadly.fbx"),
-  "script": Script.resolvePath("bow.js") + "?" + Date.now(),
-  "lifetime": 600, // In seconds. Other than -1 means it will eventually disappear
-  "position": { // There's probably a Vec3 method that would be better. Base it off the front of the avatar?
-    "x": MyAvatar.position.x,
-    "y": MyAvatar.position.y + 2,
-    "z": MyAvatar.position.z + 2
+  name: "Hifi-Bow",
+  created: CREATION_DATE.toISOString(),
+  type: "Model",
+  modelURL: Script.resolvePath("bow-deadly.fbx"),
+  script: Script.resolvePath("bow.js") + "?" + Date.now(),
+  lifetime: 600, // In seconds. Other than -1 means it will eventually disappear
+  position: { // There's probably a Vec3 method that would be better. Base it off the front of the avatar?
+    x: MyAvatar.position.x,
+    y: MyAvatar.position.y + 2,
+    z: MyAvatar.position.z + 2
   }, // Position is required. Otherwise it won't rez.
   //"rotation": Quat.fromPitchYawRollDegrees(0, 90, 0), // If entity spawns intersecting floor, it will fall through.
-  "dimensions": { // Entity scaled to match and bounding box created
-    "x": 0.0400,
-    "y": 1.3000,
-    "z": 0.2000
+  dimensions: { // Entity scaled to match and bounding box created
+    x: 0.0400,
+    y: 1.3000,
+    z: 0.2000
   },
-  "gravity": {
-    "x": 0,
-    "y": -9.8,
-    "z": 0
+  gravity: {
+    x: 0,
+    y: -9.8,
+    z: 0
   },
-  "collisionsWillMove": 1,
-  "shapeType": "compound",
-  "compoundShapeURL": Script.resolvePath("bow_collision_hull.obj"),
-  "dynamic": 1, // Will be moved by heavier objects
-  "userData": JSON.stringify(userData),
-  "velocity": {
-    "x": 0,
-    "y": -0.01, // Just so gravity has an effect
-    "z": 0
+  collisionsWillMove: 1,
+  shapeType: "compound",
+  compoundShapeURL: Script.resolvePath("bow_collision_hull.obj"),
+  dynamic: 1, // Will be moved by heavier objects
+  userData: JSON.stringify(userData),
+  velocity: {
+    x: 0,
+    y: -0.01, // Just so gravity has an effect
+    z: 0
   }
 });
 
